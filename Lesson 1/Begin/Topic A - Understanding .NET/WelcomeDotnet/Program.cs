765e102ec466fa259daa63f6c40cc1ba99211752
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-10-19 20:20:26 -0400

    Upload

---
+++ Lesson 1/Begin/Topic A - Understanding .NET/WelcomeDotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World");
+
+            System.Console.WriteLine("Welcome, .Net Core");
+        }
+    }
+}
---
+++ Lesson 1/Begin/Topic A - Understanding .NET/firstPrg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstPrg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rise & Grind!");
+        }
+    }
+}
