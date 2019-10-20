a5998d07d13981a6de4abf061db4e017d09bd397
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-10-20 09:50:25 -0400

    Basics - First Commit

---
+++ Lesson 2/Begin/Topic B - Declaring Variables/Basics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Person;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WriteStatement();
+            //SaySomething("Rey");
+            Female female = new Female();
+            female.Name = "Cindy";
+            Console.WriteLine($"Her name is {female.Name}");
+
+        }
+
+        //OVERLOADING
+        //Demonstration for overloading
+  
+        static void SaySomething()
+        {
+            Console.WriteLine("I don't even know your name. Don't order me");
+        }
+
+        static void SaySomething(string first)
+        {
+            Console.WriteLine($"Cheers, {first}");
+        }
+
+        static void SaySomething(string first, string last)
+        {
+            Console.WriteLine($"Cheers, {first} {last}");
+        }
+
+        //This method will write a statement
+        private static void WriteStatement()
+        {
+            var firstName = "Cornelia";
+            var lastName = "Johnson";
+            var fullName = firstName + " " + lastName;
+            Console.Write("My name is " + fullName);
+        }
+    }
+}
+
+namespace Person
+{
+    public class Female
+    {
+        public string Name {get; set;}
+    }
+}
