945f5dc705b38b4a9ac97c4dac12c180017a0fb3
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-10-25 18:47:01 -0400

    Replace WebAppMvc

--- Lesson 7/End/Topic A - Understanding ASP.NET Core/WebAppMVC/Controllers/HomeController.cs
+++ Lesson 7/End/Topic A - Understanding ASP.NET Core/WebAppMVC/Controllers/HomeController.cs
@@ -4,74 +4,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Packt.CS7;
+using Microsoft.Extensions.Logging;
 using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private Northwind db;
-        public HomeController(Northwind injectedContext)
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            db = injectedContext;
+            _logger = logger;
         }
-        public IActionResult ProductsThatCostMoreThan(decimal? price)
-        {
-            if (!price.HasValue)
-            {
-                return NotFound("You must pass a product price in the query string, " +
-                    "for example, / Home / ProductsThatCostMoreThan ? price = 50");
-            }
-            var model = db.Products.Include(p => p.Category).Where(p => p.UnitPrice >
-            price).ToArray();
-            if (model.Count() == 0)
-            {
-                return NotFound($"No products cost more than {price:C}.");
-            }
-            ViewData["MaxPrice"] = price.Value.ToString("C");
-            return View(model); // pass model to view
-        }
-        public IActionResult ProductDetail(int? id)
-        {
-            if (!id.HasValue)
-            {
-                return NotFound("You must pass a product ID in the " +
-                    "route, for example, / Home / ProductDetail / 21");
-            }
-            var model = db.Products.SingleOrDefault(p =>
-            p.ProductID == id);
-            if (model == null)
-            {
-                return NotFound($"A product with the ID of {id} was found.");
-            }
-            return View(model); // pass model to view
-        }
+
         public IActionResult Index()
         {
-            var model = new HomeIndexViewModel
-            {
-                VisitorCount = (new Random()).Next(1, 1001),
-                Products = db.Products.ToArray()
-            };
-            return View(model); // pass model to view
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Privacy()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Lesson 7/End/Topic A - Understanding ASP.NET Core/WebAppMVC/Models/HomeIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace Packt.CS7
-{
-    public class HomeIndexViewModel
-    {
-        public int VisitorCount;
-        public ICollection<Product> Products { get; set; }
-    }
-}
